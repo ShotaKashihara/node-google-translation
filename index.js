75c0165357c501cf83a6a2a5b4b4a38fa14a98fa
Shota Kashihara <mercury4th@gmail.com>
2020-01-13 22:55:26 +0900

    init and end

---
+++ index.js
@@ -0,0 +1,36 @@
+/**
+ * TODO(developer): Uncomment these variables before running the sample.
+ */
+const projectId = 'kirin-0';
+const location = 'global';
+
+// Imports the Google Cloud Translation library
+const {TranslationServiceClient} = require('@google-cloud/translate').v3beta1;
+const fs = require('fs')
+
+const json = JSON.parse(fs.readFileSync('input/composer2.json', 'utf8'))
+
+// Instantiates a client
+const translationClient = new TranslationServiceClient();
+async function translateText() {
+  // Construct request
+  const request = {
+    parent: translationClient.locationPath(projectId, location),
+    contents: json,
+    mimeType: 'text/plain', // mime types: text/plain, text/html
+    sourceLanguageCode: 'en-US',
+    targetLanguageCode: 'ja-JP',
+  };
+
+  // Run request
+  const [response] = await translationClient.translateText(request);
+
+  fs.writeFileSync('output/composer2.json', JSON.stringify(response, null, 2), 'utf8')
+
+  for (const translation of response.translations) {
+    console.log(`Translation: ${translation.translatedText}`);
+  }
+}
+
+// console.log(json)
+translateText();
